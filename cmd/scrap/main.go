5212871a4563d8e4efbe65e8d914a701c5be61f5
Dalton Gomes Lobato <104688487+dalton02@users.noreply.github.com>
2025-12-16 18:21:10 -0300

    feat: complementa o comando de scrap

--- cmd/scrap/main.go
+++ cmd/scrap/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	document_service "licor_model/core/modules/document/service"
 	scrapper_service "licor_model/core/modules/scrapper/service"
+	"licor_model/core/server"
+	"licor_model/core/server/shared"
 )
 
 func main() {
-
+	database, err := server.InitConnection()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer database.Close()
+	shared.SetDB(database)
 	docService := document_service.NewDocumentService()
 
 	scrapperService := scrapper_service.NewScrapperService(docService)
